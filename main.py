e973b9d5d5c40368afefde1ab87d286a140d1c12
Kelvin <hello@iamkelv.in>
2015-09-18 19:43:36 +0100

    Add attack input handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from time import sleep
+
+import yaml
+
 from player import Player
 from attack import Attack
-import yaml
 
 
 def main():
@@ -12,12 +14,10 @@ def main():
     attack_config = yaml.load(f)
     f.close()
 
-
     # Load attacks
-    attacks = []
+    attacks = {}
     for name, properties in attack_config.items():
-        attacks.append(Attack(name, int(properties['strength']), int(properties['accuracy'])))
-
+        attacks[name] = (Attack(name, int(properties['strength']), int(properties['accuracy'])))
 
     # Introduce the game
     print("""
@@ -33,15 +33,65 @@ def main():
     player_two = Player(input("What is Player 2's name? "))
 
     # Start the game
-    print()
-    print("{!s} challenges {!s} to a python battle!".format(player_one.name, player_two.name))
+    print("\n{!s} challenges {!s} to a python battle!\n".format(player_one.name, player_two.name))
     sleep(0.5)
 
+    # Assign players to current/target players
+    current_player = player_one
+    target_player = player_two
+
     # Start a round
-    while player_one.health < 0 and player_two.health < 0:
+    while player_one.health > 0 and player_two.health > 0:
+        # Output player's turn
+        sleep(0.5)
+        print("It's {!s}'s turn --\n".format(current_player.name))
+        sleep(0.5)
 
+        # Output player HPs
+        sleep(0.5)
+        print("{!s} - {!s}/100 HP".format(player_one.name, player_two.health))
+        print("{!s} - {!s}/100 HP\n".format(player_two.name, player_one.health))
+        sleep(0.5)
 
+        # Output available attacks
+        sleep(0.5)
+        print("Available attacks:")
+        print("==================")
+        for name, attack in attacks.items():
+            print("{!s} - Strength: {!s} - Accuracy: {!s}".format(attack.name, attack.strength, attack.accuracy))
+        print("==================\n")
+        sleep(0.5)
 
+        # User inputs attack
+        while True:
+            input_attack = input("{!s}, enter the name of the attack you want to use: ".format(current_player.name))
+            if input_attack in attacks:
+                chosen_attack = attacks[input_attack]
+                break
+            else:
+                print("The selected attack is invalid. Try again!\n")
+
+        # Show chosen attack
+        sleep(1)
+        # noinspection PyUnboundLocalVariable
+        print("\n{!s} chose {!s}!".format(current_player.name, chosen_attack.name))
+        sleep(1)
+
+        # Update player objects and switch around current/target players
+        if current_player.name == player_one.name:
+            player_one = current_player
+            player_two = target_player
+
+            current_player = player_two
+            target_player = player_one
+        else:
+            player_two = current_player
+            player_one = target_player
+
+            current_player = player_one
+            target_player = player_one
+
+        break
 
 
 if __name__ == '__main__':
