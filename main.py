263803feaa922f86ebf87dc3eafd816374bcca4d
Kelvin <hello@iamkelv.in>
2015-09-18 19:00:27 +0100

    Add attack config loading

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
+from time import sleep
 from player import Player
 from attack import Attack
+import yaml
+
 
 def main():
+    # Load YAML config
+    config_path = "./attack.yml"
+    f = open(config_path, 'r')
+    attack_config = yaml.load(f)
+    f.close()
+
+
+    # Load attacks
+    attacks = []
+    for name, properties in attack_config.items():
+        attacks.append(Attack(name, int(properties['strength']), int(properties['accuracy'])))
+
+
     # Introduce the game
     print("""
     PyBattle
@@ -16,7 +32,16 @@ def main():
     player_one = Player(input("What is Player 1's name? "))
     player_two = Player(input("What is Player 2's name? "))
 
+    # Start the game
+    print()
     print("{!s} challenges {!s} to a python battle!".format(player_one.name, player_two.name))
+    sleep(0.5)
+
+    # Start a round
+    while player_one.health < 0 and player_two.health < 0:
+
+
+
 
 
 if __name__ == '__main__':
--- player.py
+++ player.py
@@ -1,3 +1,7 @@
 class Player(object):
-    def __init__(self, name):
+    def __init__(self, name, health=100):
         self.name = name
+        self.health = health
+
+    def deplete(self, points):
+        self.health = self.health - points
